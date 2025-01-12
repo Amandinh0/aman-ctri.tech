6710607f6a289288d42d0e0ed70aac218aa91f11
Amandinh0 <amanchhetricr7@gmail.com>
2025-01-12 17:23:37 -0500

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,31 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aman's Personal Website",
   description: "Aman Chhetri Personal Webiste Portfolio",
-  
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
